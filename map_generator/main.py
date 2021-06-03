7055396b7fcb9410c5f0eb73cddd607c52a08ea2
adwojak <dwojaka207@gmail.com>
2021-06-03 20:23:44 +0200

    Added exit room

--- map_generator/main.py
+++ map_generator/main.py
@@ -9,8 +9,9 @@ MIDDLE = "MIDDLE"
 OPPOSITE_FACES = {UP: DOWN, DOWN: UP, LEFT: RIGHT, RIGHT: LEFT}
 
 STARTING = "STARTING"
+EXIT = "EXIT"
 
-ROOM_TYPES = {STARTING: "S"}
+ROOM_KINDS = {STARTING: "S", EXIT: "E"}
 
 
 class Room:
@@ -20,11 +21,10 @@ class Room:
     symbol = None
     face = None
 
-    def __init__(self, x_position, y_position, kind, symbol, face=None):
+    def __init__(self, x_position, y_position, face=None):
         self.x_position = x_position
         self.y_position = y_position
-        self.kind = kind
-        self.symbol = symbol
+        self.symbol = ROOM_KINDS[self.kind]
         self.face = face or MIDDLE
 
     @property
@@ -33,11 +33,14 @@ class Room:
 
 
 class StartingRoom(Room):
-    room_type = STARTING
-    symbol = ROOM_TYPES[room_type]
+    kind = STARTING
 
     def __init__(self, x_position, y_position, face):
-        super().__init__(x_position, y_position, self.room_type, self.symbol, face)
+        super().__init__(x_position, y_position, face)
+
+
+class ExitRoom(StartingRoom):
+    kind = EXIT
 
 
 class MapGenerator:
@@ -51,23 +54,32 @@ class MapGenerator:
     generated_map = None
     rooms = {}
 
-    def randomize_position_on_face(self):
+    def randomize_position_on_any_face(self):
         x_position = randint(self.START, self.END)
         if x_position in [self.START, self.END]:
             return [x_position, randrange(self.START + 1, self.END)]
         return [x_position, choice([self.START, self.END])]
 
+    def randomize_position_on_face(self, face):
+        single_coordinate = randint(self.START + 1, self.END - 1)
+        return {
+            DOWN: (single_coordinate, self.END),
+            UP: (single_coordinate, 0),
+            LEFT: (0, single_coordinate),
+            RIGHT: (self.END, single_coordinate),
+        }[face]
+
     def generate_entrance(self):
-        starting_x, starting_y = self.randomize_position_on_face()
+        starting_x, starting_y = self.randomize_position_on_any_face()
         return StartingRoom(
             starting_x, starting_y, self.get_face(starting_x, starting_y)
         )
 
-    def generate_empty_map(self):
-        return [["."] * self.SQUARE_SIZE for _ in range(self.SQUARE_SIZE)]
-
-    def generate_rooms(self):
-        self.rooms[STARTING] = self.generate_entrance()
+    def generate_exit(self):
+        starting_room_face = self.rooms[STARTING].face
+        exit_room_face = OPPOSITE_FACES[starting_room_face]
+        exit_x, exit_y = self.randomize_position_on_face(exit_room_face)
+        return ExitRoom(exit_x, exit_y, exit_room_face)
 
     def get_face(self, x_position, y_position):
         if x_position == 0:
@@ -79,8 +91,15 @@ class MapGenerator:
         elif y_position == self.END:
             return DOWN
 
+    def generate_empty_map(self):
+        return [["."] * self.SQUARE_SIZE for _ in range(self.SQUARE_SIZE)]
+
+    def generate_rooms(self):
+        self.rooms[STARTING] = self.generate_entrance()
+        self.rooms[EXIT] = self.generate_exit()
+
     def insert_rooms_into_map(self):
-        for room_type, room in self.rooms.items():
+        for kind, room in self.rooms.items():
             self.generated_map[room.y_position][room.x_position] = room.symbol
 
     def generate_map(self):
